c11ab887efb13aee05a6c417bf30846e9d443722
dawid <dawid.zzzak@gmail.com>
2024-04-26 19:30:32 +0200

    test

--- ASD/zad5/zad5.py
+++ ASD/zad5/zad5.py
@@ -2,12 +2,10 @@ from zad5testy import runtests
 from queue import PriorityQueue
 
 def spacetravel( n, E, S, a, b ):
-    n = max(E, key=lambda x: x[1])[1] + 1
     neighbours = [[] for _ in range(n)]
     isanomaly = [False]*n
     for el in S:
         isanomaly[el] = True
-
     if a in S and b in S:
         return 0
     elif a in S:
@@ -21,34 +19,28 @@ def spacetravel( n, E, S, a, b ):
             E[i] = (name, E[i][1], E[i][2])
         if isanomaly[E[i][1]]:
             E[i] = (E[i][0], name, E[i][2])
-    brolist = [False]*n
     broweight = [float("inf")]*n
-
     for i in range(len(E)):
         if isanomaly[E[i][0]]:
-            brolist[E[i][1]] = True
             broweight[E[i][1]] = min(E[i][2],broweight[E[i][1]])
         elif isanomaly[E[i][1]]:
-            brolist[E[i][0]] = True
             broweight[E[i][0]] = min(E[i][2],broweight[E[i][0]])
         else:
             neighbours[E[i][0]].append([E[i][1], E[i][2]])
             neighbours[E[i][1]].append([E[i][0], E[i][2]])
-    for i in range(len(brolist)):
-        if brolist[i]:
+    for i in range(len(broweight)):
+        if broweight[i] != float("inf"):
             neighbours[name].append([i, broweight[i]])
             neighbours[i].append([name, broweight[i]])
     q = PriorityQueue()
     distance = [float("inf")]*(len(neighbours) + 1)
     q.put((0, a))
     distance[a] = 0
-
     def relax(distance, v, w, u):
         if distance[v] > distance[u] + w:
             distance[v] = distance[u] + w
             return True
         return False
-
     while not q.empty():
         dist, u = q.get()
         for v, w in neighbours[u]:
